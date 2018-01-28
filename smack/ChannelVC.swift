befa57ed22ce228ceafb8fe752c4c414c3dacf95
Sebastian Salamanca <sebastiansalamanca@aol.com>
2018-01-28 14:55:01 -0500

    Channles set up

--- smack/ChannelVC.swift
+++ smack/ChannelVC.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ChannelVC: UIViewController {
+class ChannelVC: UIViewController, UITableViewDelegate,UITableViewDataSource {
     
     // Outlets
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var logBtn: UIButton!
     @IBOutlet weak var userImg: CircleImage!
     @IBAction func prepareForUnwind(segue:UIStoryboardSegue){}
@@ -18,7 +19,9 @@ class ChannelVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
@@ -64,4 +67,30 @@ class ChannelVC: UIViewController {
     @objc func userDataDidChange(_ notif: Notification){
         setUpUserInfo()
     }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
+            
+            let channel = MessageService.instance.channels[indexPath.row]
+            
+            cell.configureCell(channel: channel)
+            return cell
+        
+        }else{
+            return UITableViewCell()
+        
+        }
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
+    }
+    
 }
